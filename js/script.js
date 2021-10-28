0a4583707a867420addac97b343939840f6bd490
faridzwahyu <faridzwahjoe@gmail.com>
2021-10-28 18:23:25 +0700

    Make style

---
+++ js/script.js
@@ -0,0 +1,63 @@
+const showToggle = document.querySelector(".show"),
+   createPass = document.querySelector("#create"),
+   confirmPass = document.querySelector("#confirm"),
+   alertIcon = document.querySelector(".error-icon"),
+   alertText = document.querySelector(".text");
+
+// ========== SHOW HIDDEN PASSWORD ==========
+showToggle.addEventListener("click", () => {
+   if (createPass.type === "password" && confirmPass.type === "password") {
+      createPass.type = "text";
+      confirmPass.type = "text";
+      showToggle.classList.replace("fa-eye-slash", "fa-eye");
+      showToggle.style.color = "#4070f4";
+   } else {
+      createPass.type = "password";
+      confirmPass.type = "password";
+      showToggle.classList.replace("fa-eye", "fa-eye-slash");
+      showToggle.style.color = "#a6a6a6";
+   }
+});
+
+// ========== LENGTH PASSWORD VALIDATION ==========
+createPass.addEventListener("input", () => {
+   let createValue = createPass.value.trim();
+
+   if (createValue == "") {
+      alertText.innerText = "Enter atleast 8 character";
+      alertIcon.style.display = "none";
+   } else if (createValue.length < 8) {
+      alertText.innerText = "Password too short";
+      alertIcon.style.display = "block";
+      alertIcon.style.color = "#d93025"; //merah
+      alertIcon.classList.replace("fa-check-circle", "fa-exclamation-circle");
+   } else {
+      alertText.innerText = "Very good";
+      alertIcon.style.display = "block";
+      alertIcon.style.color = "#32CD32"; //hijau
+      alertIcon.classList.replace("fa-exclamation-circle", "fa-check-circle");
+   }
+});
+
+// ========== CONFIRM PASSWORD VALIDATION ==========
+confirmPass.addEventListener("input", () => {
+   let createValue = createPass.value.trim();
+   let confirmValue = confirmPass.value.trim();
+
+   if (confirmValue === createValue && createValue.length >= 8) {
+      alertText.innerText = "Password matched";
+      alertIcon.style.display = "block";
+      alertIcon.style.color = "#32CD32"; //hijau
+      alertIcon.classList.replace("fa-exclamation-circle", "fa-check-circle");
+   } else if (createValue.length < 8) {
+      alertText.innerText = "Password too short";
+      alertIcon.style.display = "block";
+      alertIcon.style.color = "#d93025"; //merah
+      alertIcon.classList.replace("fa-check-circle", "fa-exclamation-circle");
+   } else {
+      alertText.innerText = "Password didn't matched";
+      alertIcon.style.display = "block";
+      alertIcon.style.color = "#d93025"; //merah
+      alertIcon.classList.replace("fa-check-circle", "fa-exclamation-circle");
+   }
+});
